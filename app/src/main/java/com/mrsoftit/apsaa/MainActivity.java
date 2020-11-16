af05988c28cb24ae7f6ff7eb9cee6b18400f915f
mijantck <mijantck3.ofic@gmail.com>
2020-11-16 18:15:54 +0600

    try

---
+++ app/src/main/java/com/mrsoftit/apsaa/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mrsoftit.apsaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
